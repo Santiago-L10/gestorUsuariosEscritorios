881b08bc51bcb45fc6f019396775508bc70f293d
Santiago-L10 <santiagoleon009@gmail.com>
2025-04-30 19:37:17 -0500

    Update Person.cs

--- GestorUsuarios/GestorUsuarios/models/Person.cs
+++ GestorUsuarios/GestorUsuarios/models/Person.cs
@@ -2,6 +2,11 @@
 {
     class Person
     {
+        public int IdPerson { get; set; }
+        public string NamesPerson { get; set; }
+        public string LastNamesPerson { get; set; }
+        public int AgePerson { get; set; }
+        public string emailPerson { get; set; }
 
     }
 }
