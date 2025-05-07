9c21805c7b4233c70f4b0d52e593488cebda0df0
Santiago.L <santiagoleon009@gmail.com>
2025-05-07 15:50:16 -0500

    commit files assembly

--- GestorUsuarios/GestorUsuarios/obj/Debug/net8.0-windows/GestorUsuarios.AssemblyInfo.cs
+++ GestorUsuarios/GestorUsuarios/obj/Debug/net8.0-windows/GestorUsuarios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorUsuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2938b7bc5d6ec6d043c7a5606a122cded06f92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538484b07bf5ccb73fd03da015dd95a0ab95178b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorUsuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorUsuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
