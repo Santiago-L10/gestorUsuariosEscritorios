b82d34612880b14b2d6bdd0fcecf91584457b606
pipejfdv <juampi_03_33@hotmail.com>
2025-05-17 11:47:40 -0500

    constructor de person creado

--- GestorUsuarios/GestorUsuarios/models/Person.cs
+++ GestorUsuarios/GestorUsuarios/models/Person.cs
@@ -8,5 +8,17 @@
         public int AgePerson { get; set; }
         public string EmailPerson { get; set; }
 
+        public Person() { }
+        public Person(int idPerson, string namePerson, string lastnamesPerson, int age, string emailPerson)
+        {
+            IdPerson = idPerson;
+            NamesPerson = namePerson;
+            LastNamesPerson = lastnamesPerson;
+            AgePerson = age;
+            EmailPerson = emailPerson;
+        }
+
     }
+
+    
 }
